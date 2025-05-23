c97985603d7dc0a47a3b9c235fe5ee5f074202a0
Bastian Becerra <bastianbecerrabasoalto@gmail.com>
2025-05-23 11:56:17 -0400

    Agregado de archivos al repositorio

---
+++ src/test/java/com/grupo7/Perfulandia/PerfulandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo7.Perfulandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
